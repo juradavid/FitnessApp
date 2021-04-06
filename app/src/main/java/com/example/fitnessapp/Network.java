f50e2aa4d5d606793116630276ac7bdabceefed0
juradavid <79250135+juradavid@users.noreply.github.com>
2021-04-07 01:30:38 +0300

    Concept v0.1

--- app/src/main/java/com/example/fitnessapp/Network.java
+++ app/src/main/java/com/example/fitnessapp/Network.java
@@ -6,17 +6,13 @@ import android.net.NetworkInfo;
 
 public class Network {
     public static boolean isConnectedToInternet(Context context){
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        if(cm != null){
-            NetworkInfo[] info = cm.getAllNetworkInfo();
-            if(info != null){
-                for(int i = 0; i < info.length; i++){
-                    if(info[i].getState() == NetworkInfo.State.CONNECTED)
-                        return true;
-                }
-            }
-        }
-        return false;
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        return isConnected;
     }
 }
--- app/src/main/java/com/example/fitnessapp/second_page.java
+++ app/src/main/java/com/example/fitnessapp/second_page.java
@@ -15,16 +15,37 @@ import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.UnderlineSpan;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.muddzdev.styleabletoast.StyleableToast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URLDecoder;
 
 public class second_page extends AppCompatActivity {
-
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
 
     TextView sHeader;
     CheckBox checkBox;
+    Button registerButton;
+
+    EditText username;
+    EditText email;
+    EditText password;
+    EditText phone;
 
     int isActive = 0;
 
@@ -36,6 +57,11 @@ public class second_page extends AppCompatActivity {
         sharedPreferences = getSharedPreferences(MainActivity.SHARED_PREFS, MODE_PRIVATE);
         sHeader = findViewById(R.id.sHeader);
         checkBox = findViewById(R.id.checkBox);
+        registerButton = findViewById(R.id.registerButton);
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        phone = findViewById(R.id.phone);
 
         String tempText = (String) sHeader.getText();
         SpannableString ssHeader = new SpannableString(tempText);
@@ -87,6 +113,17 @@ public class second_page extends AppCompatActivity {
                     Intent.FLAG_ACTIVITY_NO_ANIMATION);
             startActivity(intent);
         }
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkBox.isChecked()){
+                        register();
+                }else{
+                    StyleableToast.makeText(getApplicationContext(),"Check the information your provided", R.style.noInternetConnection, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void content(){
@@ -122,6 +159,46 @@ public class second_page extends AppCompatActivity {
         };
 
         fHandler.postDelayed(fRunnable,miliseconds);
+    }
+
+    public void register(){
+        String postUrl = "http://167.99.246.91/fitnessapp/register.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("username", username.getText().toString());
+            postData.put("email", email.getText().toString());
+            postData.put("password", password.getText().toString());
+            postData.put("phone", phone.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, postUrl, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    System.out.println(response.getString("result"));
+                    if(response.getString("result").equals("trimis")){
+                        StyleableToast.makeText(getApplicationContext(),"Contul a fost creat cu succes", Toast.LENGTH_SHORT).show();
+                    }
+                    else if(response.getString("result").equals("inregistrat")){
+                        StyleableToast.makeText(getApplicationContext(),"Email-ul este deja folosit", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error);
+                StyleableToast.makeText(getApplicationContext(),"Este o problema la server", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
     }
 
     @Override
